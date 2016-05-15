20b00a8c74986342e54a327abf8c5930f3c285be
Carter <hamming1@gmail.com>
2016-05-15 09:06:00 +0800

    Fixed encrypted algorithm

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+    "crypto/rand"
+    "crypto/rsa"
+    "crypto/x509"
+    "encoding/base64"
+    "encoding/pem"
+    "fmt"
+    "io/ioutil"
+    )
 
 func main() {
-    fmt.Printf("kkk")
-    fmt.Println("Hello")
+    
+    var pub *rsa.PublicKey
+    random := rand.Reader
+    data, err := ioutil.ReadFile("./public.pem")
+    
+    if err != nil {
+        panic(err)
+    }
+    
+    block, _ := pem.Decode(data)
+    msg := []byte("hello")
+    
+    pubInterface, parseErr := x509.ParsePKIXPublicKey(block.Bytes)
+    
+    if parseErr != nil {
+        fmt.Println("Load public key error")
+        panic(parseErr)
+    }
+    
+    pub = pubInterface.(*rsa.PublicKey)
+    
+    encryptedData, encryptedErr := rsa.EncryptPKCS1v15(random, pub, msg)
+    fmt.Println(encryptedErr)
+    fmt.Println(base64.URLEncoding.EncodeToString(encryptedData))
 }
